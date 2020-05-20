568f83da0a424b7b823db86f8b2d88564fa33e46
Ricardo <ricardojonasdasilva60@gmail.com>
2020-05-20 07:04:35 -0300

    subindo alteracoes

---
+++ src/app/usuario/ativarConta/ativar-conta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'ativar-conta',
+  templateUrl:'ativar-conta.component.html',
+  styleUrls:['ativar-conta.component.css']
+})
+export class AtivarContaComponent{
+
+}
